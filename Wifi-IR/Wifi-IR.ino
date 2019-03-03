2a1bac4ccdc2e8103266d4e05f63e32e0bd92eb8
Niels de Klerk <mail@nielsdeklerk.nl>
2019-03-03 14:15:56 +0100

    V0.2
    Added TCP handeling.
    Connect to IP TCP port 23.
    Send 0000 0000 0000 0000 .... .... \r

--- Wifi-IR/Wifi-IR.ino
+++ Wifi-IR/Wifi-IR.ino
@@ -6,25 +6,25 @@
 #include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
 
 // Configure Ditigal PIN
-// D4 is used because it is next to the ground pin on Wemos D1 Mini.
-IRsend irsend(D4);
+// D3 is used with the wemos IR shield: https://wiki.wemos.cc/products:d1_mini_shields:ir_controller_shield
+IRsend irsend(D3);
 char* hostname = "Wifi-IR";
 
 ProntoHex ph = ProntoHex();       //Prontohex.
-ESP8266WebServer server(80);      //default web service.
+ESP8266WebServer serverHttp(80);  //HTTP service.
+WiFiServer serverTcp(23);         //TCP service.
 
 void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
+  Serial.println(F("Entered config mode"));
   Serial.println(WiFi.softAPIP());
   Serial.println(myWiFiManager->getConfigPortalSSID());
 }
 
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  
   WiFiManager wifiManager;
-  //wifiManager.resetSettings();
+  //wifiManager.resetSettings(); // Remove stored WiFi Settings.
 
   wifiManager.setAPCallback(configModeCallback);
   if(!wifiManager.autoConnect(hostname)) {
@@ -33,34 +33,78 @@ void setup() {
     delay(1000);
   } 
   
-  Serial.print("Connected to wifi. Using IP: ");
+  Serial.print(F("Connected to wifi. Using IP: "));
   Serial.println(WiFi.localIP());
 
   // Start ir library
   irsend.begin();
 
   // Start the server
-  server.on("/", handleRoot);
-  server.on("/pronto", handlePronto);
-  server.onNotFound(handleRoot);
-  server.begin();
-  Serial.println("Server started");
+  serverHttp.on("/", handleHttpRoot);
+  serverHttp.on("/pronto", handleHttpPronto);
+  serverHttp.onNotFound(handleHttpRoot);
+  serverHttp.begin();
+  Serial.println("Http Server started");
+
+
+  serverTcp.begin();
 }
 
 void loop() {
-  server.handleClient();
-  // END
+  serverHttp.handleClient();
+  handleTcpPronto();
 }
 
-void handleRoot() {
- server.send(200, "text/html", "<body><h1>Wifi-IR, send Pronto HEX formater infrared signals!</h1><form onsubmit=\"window.open('/pronto?code=' + document.getElementById('pronto').value, '_new')\"> Pronto HEX:<br> <input type=\"text\" name=\"code\" id=\"pronto\"><br> <input type=\"submit\" value=\"Send\"> </form> <p> Find IR codes at: <a href=\"http://irdb.tk/find/\">irdb.tk</a> </p></body>");
+void handleHttpRoot() {
+ serverHttp.send(200, "text/html", "<body><h1>Wifi-IR, send Pronto HEX formater infrared signals!</h1><form onsubmit=\"window.open('/pronto?code=' + document.getElementById('pronto').value, '_new')\"> Pronto HEX:<br> <input type=\"text\" name=\"code\" id=\"pronto\"><br> <input type=\"submit\" value=\"Send\"> </form> <p> Find IR codes at: <a href=\"http://irdb.tk/find/\">irdb.tk</a> </p></body>");
 }
 
-void handlePronto(){
-  for (uint8_t i=0; i<server.args(); i++){
-    if(server.argName(i) == "code") 
+void handleTcpPronto(){
+  WiFiClient tcpClient = serverTcp.available();
+  
+  if (tcpClient) {
+    if(tcpClient.connected())
     {
-      String line = server.arg(i);
+      Serial.println("Client Connected");
+    }
+    
+    while(tcpClient.connected()){      
+      while(tcpClient.available()>0){
+        // read data from the connected client
+        String pronto = tcpClient.readStringUntil('\r');
+        boolean didSend = handlePronto(pronto);
+        if (didSend) {
+          //tcpClient.write("1\r");
+        } else {
+          //tcpClient.write("Error!\r\n");
+        }
+      }
+      //Send Data to connected client
+      while(Serial.available()>0)
+      {
+        tcpClient.write(Serial.read());
+      }
+    }
+    tcpClient.stop();
+    Serial.println("Client disconnected");    
+  }
+}
+
+void handleHttpPronto(){
+  for (uint8_t i=0; i<serverHttp.args(); i++){
+    if(serverHttp.argName(i) == "code") 
+    {
+      boolean didSend = handlePronto(serverHttp.arg(i));
+      if (didSend) {
+        serverHttp.send(200, "text/html", "OK");
+      } else {
+        serverHttp.send(200, "text/html", "ERROR");
+      }
+    }
+  }
+}
+
+boolean handlePronto(String line){
       line.trim();
       if (line.startsWith("0000 ")) // Then we assume it is Pronto Hex
       {
@@ -72,10 +116,8 @@ void handlePronto(){
         Serial.println(ph.length);
         Serial.println("irsend.sendRaw()");
         irsend.sendRaw(ph.convertedRaw, ph.length, ph.frequency);
-        server.send(200, "text/html", "OK");
+        return true;
       } else {
-        server.send(200, "text/html", "ERROR");
+        return false;
       }
-    }
-  }
 }
